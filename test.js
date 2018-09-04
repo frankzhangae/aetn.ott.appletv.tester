a0cabb4c4cc3596b1d728528504cbd1cf357bb02
Frank <fzhang@aetvn.com>
2018-09-04 10:44:20 -0400

    separated brands and configs, get ready for video and show testing

--- test.js
+++ test.js
@@ -1,92 +1,95 @@
-// config 
-const brands = ['mlt'];
-//const brands = ['mlt', 'history', 'fyi', 'ae'];
-const configs = {
-	"mlt": require('./config/config-mlt.json')
-};
-//const configs = {'history': require('./config/config-test.json')};
-
 // lib
 String.prototype.replaceAll = function(search, replacement) {
     var target = this;
     return target.split(search).join(replacement);
 };
-
+// Modules
 const request = require('request');
 const colors = require('colors');
-//text colors: black red green yellow blue magenta cyan white gray grey
+const _ = require('underscore');
+// System configs
 colors.setTheme({
+	//text colors: black red green yellow blue magenta cyan white gray grey
 	category: 'yellow',
 	ae: 'blue',
 	fyi: 'green',
 	history: 'red',
 	mlt: 'magenta',
-	version: 'yellow',
+	param: 'yellow',
 	info: 'cyan',
 	pass: 'green',
 	warn: 'yellow',
 	debug: 'blue',
 	error: 'red'
 });
-const _ = require('underscore');
 /* 
  * main()
- * node writeRoutesResult [laravel ver] [php version] [env] ...[brand]
- * @param laravel version: mandatory
- * @param php version: 5.6
- * @param env: dev
- * @param brand: all four brands
+ * node writeRoutesResult [brand] [laravel ver] [php version] [env] 
+ * @param brand: one of (history, mlt, fyi, ae)
+ * @param laravel version: !mandatory
+ * @param php version: one of (5.6, 7.0) - by default 5.6
+ * @param env: one of (dev, local, qa) - by default dev
  */
-const laravelVer = process.argv[2];
-const phpVer = process.argv[3] || '5.6';
-const env = process.argv[4] || 'dev';
-const timestamp = new Date().getTime();
+// global var
+	const brand = process.argv[2];
+	const laravelVer = process.argv[3];
+	const phpVer = process.argv[4] || '5.6';
+	const env = process.argv[5] || 'dev';
+	//const timestamp = new Date().getTime();
+	const baseUrl = 'http://' + env + '-appletv.aetndigital.com/' + brand;
+	const prodBaseUrl = 'http://appletv.aetndigital.com/' + brand;
 
+	let config = require('./config/config-' + brand + '.json');
+	//let config = require('./config/config-test.json');
+
+	let allRoutes = config["all-routes"];
+	let mvpds = config.mvpd;
+// end global var
+
+
+// main
 // use async and await to make sure the sequence
 async function main() {
+	if(!laravelVer || !brand) {
+		console.error('Params error');
+		return;
+	}
+
 	console.log('---------------------- PARAMS ----------------------');
-	console.log('Laravel:', colors.version(laravelVer), 'PHP:', colors.version(phpVer), 'Env:', colors.version(env));
+	console.log('brand:', colors.param(brand), 'Laravel:', colors.param(laravelVer), 'PHP:', colors.param(phpVer), 'Env:', colors.param(env));
 	console.log('---------------------- Start Testing ---------------');
 	console.log();
-
-	brands.forEach(brand => {
-		let pendingHyphens = new Array(29 - brand.length).join('-');
-		console.log("--------------- Brand:",  colors[brand](brand), pendingHyphens);
-
-		const baseUrl = 'http://' + env + '-appletv.aetndigital.com/' + brand;
-		const prodBaseUrl = 'http://appletv.aetndigital.com/' + brand;
-		
-		let config = configs[brand];
-
-		let allRoutes = config["all-routes"];
-		let mvpds = config.mvpd;
-		
-		testAllRoutes(allRoutes, mvpds, baseUrl, prodBaseUrl);	
-	});
+	let pendingHyphens = new Array(29 - brand.length).join('-');
+	console.log("--------------- Brand:",  colors[brand](brand), pendingHyphens);
+	 
+	testAllRoutes();	
 }
 
-async function testAllRoutes(allRoutes, mvpds, baseUrl, prodBaseUrl) {
-	for(allRoutesKey in allRoutes) {
-		let category = allRoutes[allRoutesKey];
+async function testAllRoutes() {
+	//console.log("testAllRoutes");
+
+	for(key in allRoutes) {
+		let category = allRoutes[key];
+		// console category
 		console.log("Category", colors.category(category.name));
 
-		await testGroup(category, mvpds, baseUrl, prodBaseUrl);
+		await testEachCategory(category);
 	}
 }
  
-async function testGroup(category, mvpds, baseUrl, prodBaseUrl) {
-	//console.log("testGroup");
+async function testEachCategory(category) {
+	//console.log("testEachCategory");
 
 	for(key in category.routes) {
 		let task = category.routes[key];
 
-		let result = await testEachTask(task, mvpds, baseUrl, prodBaseUrl);
-
+		let result = await testEachTask(task);
+		// console status for each task result
 		console.log(colors[result](result), task.name);
 	}
 }
 
-async function testEachTask(task, mvpds, baseUrl, prodBaseUrl) {
+async function testEachTask(task) {
 	//console.log("testEachTask");
 
 	for(key in task.urls) {
@@ -114,7 +117,9 @@ async function testEachTask(task, mvpds, baseUrl, prodBaseUrl) {
 	return "pass";
 }
 
-function testEachEndPoint(type, url1, url2) {
+function testEachEndPoint(resType, url1, url2) {
+	//console.log("testEachEndPoint");
+
 	return new Promise((resolve, reject) => {
 		request(url1, (error, res, body) => {
 			if(error || res.statusCode != 200) {
@@ -138,13 +143,6 @@ function testEachEndPoint(type, url1, url2) {
 			});
 		});
 	});
-}
-
-
-
-if(!laravelVer) {
-	console.error('no laravel version');
-	return;
 }
 
 main(); 
